64c7b8009343d7d838d7a7529daccd6725b72ebc
Yvonne <yveswangui@gmail.com>
2022-04-12 22:07:19 +0300

    commit tree 8cb80b10233e6d320cf2a2d6fafe4b600d956484

--- index.js
+++ index.js
@@ -1,1 +1,104 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+
+//APPEND
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+function destructivelyAppendCat(name){
+    return cats.push(name)
+    
+}
+destructivelyAppendCat('Ralph')
+
+//PREPEND
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+  function destructivelyPrependCat(name){
+      return cats.unshift(name)
+  }
+  destructivelyPrependCat('Bob')
+
+  //pop
+  beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+  function destructivelyRemoveLastCat(){
+      return cats.pop()
+  }
+  destructivelyRemoveLastCat()
+
+  //shift
+  beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+  function destructivelyRemoveFirstCat(){
+      return cats.shift()
+  }
+  destructivelyRemoveFirstCat()
+
+  //UNDESTRUCTIVE
+  beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+
+  function appendCat(name){
+      let appendedArray = [...cats,name]
+      return appendedArray
+  }
+  appendCat("Broom")
+
+//PREPEND
+
+  beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+  function prependCat(name){
+      let prependArray = [name,...cats]
+      return prependArray
+  }
+  prependCat()
+
+  //REMOVE LAST CAT
+  beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+
+  function removeLastCat(){
+      return cats.slice(0,2)
+  }
+  removeLastCat()
+
+  //REMOVE FIRST CAT
+  beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+  function removeFirstCat(){
+      return cats.slice(-2)
+  }
